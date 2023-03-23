dc267d157a539fd957c208228dd4d211a2702003
Venera Baiaman kyzy <venerabayaman@gmail.com>
2023-03-23 15:49:05 +0100

    'Movie' objects are instantiated

--- index.php
+++ index.php
@@ -26,3 +26,17 @@ class Movie {
   }
   
 }
+
+$movies = [
+  new Movie(
+    "Eat Pray Love", 2010, "https://serenaitalian.files.wordpress.com/2010/12/eat-pray-love-tickets.jpg", "Drama", "Ryan Murphy"
+  ),
+  new Movie(
+    "The Notebook", 2004, "https://site.unibo.it/canadausa/it/recensioni/the-notebook-e-l-amore-senza-tempo/@@eod.tiles.richtext/8d24320b6eed4bcc98b7d586232c96ad/@@images/34491dcd-3e26-4552-8705-2b5ef90d04f7.jpeg", "Drama", "	Nick Cassavetes"
+  ),
+  new Movie(
+    "The Godfather", 1972, "https://media.timeout.com/images/105455970/1372/1029/image.jpg", "Thrillers", "Francis Ford Coppola"
+  ),
+  
+];
+?>
