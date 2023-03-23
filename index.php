32b19195c1f40946da03a244e2678e6c16edb5c4
Venera Baiaman kyzy <venerabayaman@gmail.com>
2023-03-23 14:46:19 +0100

    added  _ _construct & setTitle methods to a class

--- index.php
+++ index.php
@@ -7,17 +7,22 @@ class Movie {
   public $director;
 
   public function __construct(
-    string $title,
-    int $year,
-    string $poster,
-    string $genre,
-    string $director = "unknown"    
+    string $_title,
+    int $_year,
+    string $_poster,
+    string $_genre,
+    string $_director = "unknown"    
   )  {
-    $this->title = $_title;
+    $this->setTitle($_title);
     $this->year = $_year;
     $this->poster = $_poster;
     $this->genre = $_genre;
-    $this->director = $_director;
-   
-}
+    $this->director = $_director;   
+  }
+
+  public function setTitle($title) {
+    if(!is_string($title) || $title === "") return false;
+    $this->title = $title;
+  }
+  
 }
